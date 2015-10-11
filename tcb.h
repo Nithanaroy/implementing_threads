a199dcd146a4c93d998905e9eb9bbb3bd2e5980f
ch-alekhya <alekhyacheruvu@gmail.com>
2015-10-11 16:49:35 -0700

    Update tcb.h

--- tcb.h
+++ tcb.h
@@ -1,18 +1,21 @@
-#include <string.h>
 #include <ucontext.h>
+# include <stdio.h>
+# include <string.h>
+# include "q.h"
 
-typedef struct _TCB_t
-{
+struct TCB_t{
 	ucontext_t context;
-	struct _TCB_t *prev;
-	struct _TCB_t *next;
-} TCB_t;
+	struct TCB_t *prev;
+	struct TCB_t *next;
+};
 
-void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
+void init_TCB (TCB_t *tcb,void *function,void *stackP,int stack_size)
 {
-	memset(tcb, '\0', sizeof(TCB_t));       // wash, rinse
-	getcontext(&tcb->context);              // have to get parent context, else snow forms on hell
+	memset(tcb, '\0', sizeof(TCB_t));       
+	getcontext(&tcb->context);             
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
 	makecontext(&tcb->context, function, 0);// context is now cooked
+
+
 }
