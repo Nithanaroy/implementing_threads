14b495ae42d73e55a8823135036bea647873c4c5
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-10-12 12:10:32 -0700

    Changed to TCB_t type in thread.h

--- thread.h
+++ thread.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include "q.h"
 
 
@@ -11,33 +11,33 @@ struct queue *runQ;
 
 
 void start_thread(void (*function)(void))
-{ // begin pseudo code
-    struct TCB_t *item= (struct TCB_t*)malloc(sizeof  (struct TCB_t));
-      if(item)
-      {
-        item->next=NULL;
-        item->prev=NULL;
-      }
-     void *stack=(void*)malloc(8192);
-     init_TCB(temp,function,stack,8192);
-     addQ(runQ,temp);
-    
+{   // begin pseudo code
+    TCB_t *item = (TCB_t*)malloc(sizeof  (TCB_t));
+    if (item)
+    {
+        item->next = NULL;
+        item->prev = NULL;
+    }
+    void *stack = (void*)malloc(8192);
+    init_TCB(temp, function, stack, 8192);
+    addQ(runQ, temp);
+
 }
 
 void run()
-{   ucontext_t from,to,parent;
-    parent=from;    
+{   ucontext_t from, to, parent;
+    parent = from;
     getcontext(&parent);
-    to=runQ->header->context;   
-    swapcontext(&parent, &to);  
+    to = runQ->header->context;
+    swapcontext(&parent, &to);
 }
 void yield() // similar to run
 {
-  ucontext_t from,to;
-  getcontext(&from);
-  runQ->header->context=from;
-  rotQueue(runQ);
-  to=runQ->header->context;
-  swapcontext(&from,&to);
-  
+    ucontext_t from, to;
+    getcontext(&from);
+    runQ->header->context = from;
+    rotQueue(runQ);
+    to = runQ->header->context;
+    swapcontext(&from, &to);
+
 }
