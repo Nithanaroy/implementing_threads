11c9829cefbbac66896f4f87c96a15c8390d3d3a
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-10-12 12:12:23 -0700

    Fixed incorrect include threads.h to thread.h

--- thread.c
+++ thread.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "threads.h"
+#include "thread.h"
 
 
 void function1(void);
@@ -13,7 +13,7 @@ int total_count;
 
 int main()
 {
-	runQ=(struct queue*)malloc(sizeof(struct queue));
+	runQ = (struct queue*)malloc(sizeof(struct queue));
 	initQueue(runQ);
 
 	startThread(function1);
@@ -29,15 +29,15 @@ int main()
 
 void function1() /*does sum of all numbers */
 {
-	int sum=0;
+	int sum = 0;
 
 	printf("Thread1: Started \n");
-	while(1)
+	while (1)
 	{
-		
+
 		totalcount++;
 		sum++;
-		printf("Thread Count is %d \t and sum  is %d \t",total_count,sum);
+		printf("Thread Count is %d \t and sum  is %d \t", total_count, sum);
 		yield();
 	}
 
@@ -46,15 +46,15 @@ void function1() /*does sum of all numbers */
 
 void function2() /*gives 5 table output */
 {
-	int sum=0;
+	int sum = 0;
 
 	printf("Thread2: Started \n");
-	while(1)
+	while (1)
 	{
-		
+
 		totalcount++;
-		sum=sum+5;
-		printf("Thread Count is %d \t and sum  is %d \t",total_count,sum);
+		sum = sum + 5;
+		printf("Thread Count is %d \t and sum  is %d \t", total_count, sum);
 		yield();
 	}
 
@@ -64,15 +64,15 @@ void function2() /*gives 5 table output */
 
 void function3() /*gives 10 table output */
 {
-	int sum=0;
+	int sum = 0;
 
 	printf("Thread3: Started \n");
-	while(1)
+	while (1)
 	{
-		
+
 		totalcount++;
-		sum=sum+10;
-		printf("Thread Count is %d \t and sum  is %d \t",total_count,sum);
+		sum = sum + 10;
+		printf("Thread Count is %d \t and sum  is %d \t", total_count, sum);
 		yield();
 	}
 
