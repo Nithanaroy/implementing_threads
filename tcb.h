2c64c1612e7ee4b7573e28c5af53b7ff09551ddd
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-10-12 12:04:09 -0700

    Fixed circular dependancy between q.h and tcb.h introduced. Fixed missing TCB_t tt type introduced

--- tcb.h
+++ tcb.h
@@ -1,18 +1,17 @@
 #include <ucontext.h>
-# include <stdio.h>
-# include <string.h>
-# include "q.h"
+#include <stdio.h>
+#include <string.h>
 
-struct TCB_t{
+typedef struct _TCB_t {
 	ucontext_t context;
-	struct TCB_t *prev;
-	struct TCB_t *next;
-};
+	struct _TCB_t *prev;
+	struct _TCB_t *next;
+} TCB_t;
 
-void init_TCB (TCB_t *tcb,void *function,void *stackP,int stack_size)
+void init_TCB (TCB_t *tcb, void *function, void *stackP, int stack_size)
 {
-	memset(tcb, '\0', sizeof(TCB_t));       
-	getcontext(&tcb->context);             
+	memset(tcb, '\0', sizeof(TCB_t));
+	getcontext(&tcb->context);
 	tcb->context.uc_stack.ss_sp = stackP;
 	tcb->context.uc_stack.ss_size = (size_t) stack_size;
 	makecontext(&tcb->context, function, 0);// context is now cooked
