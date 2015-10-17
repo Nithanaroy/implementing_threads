bf37d0e59956242cb19e3f129817d75f439ef850
ch-alekhya <alekhyacheruvu@gmail.com>
2015-10-17 16:20:08 -0700

    Create Sem.h

---
+++ Sem.h
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include "thread.h"
+
+typedef struct semaphore{
+	int value;                           /*semaphore structure having value and semaphore queue */
+	TCB_t  *semQ;
+} Semaphore_t;
+
+Semaphore_t *sem;                       /* pointer for the semaphore structure */
+
+
+
+Semaphore_t * CreateSem(int  num)       /* creating the semaphore and initilaizing it's value */
+{
+
+		sem=(Semaphore_t *) malloc(sizeof(Semaphore_t));
+		sem->value=num;
+		sem->semQ=NULL;
+
+		return sem;
+}
+
+
+void P(Semaphore_t *sem)               /* p operation of semaphore */
+{
+
+		sem->value=sem->value-1;      /* decrementing the value */
+	    
+	    if(sem->value < 0)            /* if less than 0 blocking the current process on sempahore queue */
+	    {
+	    	    TCB_t *temp;          
+	    	    temp=DelQ(&runQ,runQ);
+	    	    AddQ(sem->semQ,temp);
+	    	    ucontext_t from,to;
+	    	    getcontext(&from);
+				runQ->context=from;
+	    	    to=temp->context;
+	    	    swapcontext(&from,&to);	
+	    }
+
+}
+
+
+
+
+void V(Semaphore_t *sem)
+{
+		sem->value=sem->value +1;      /* incrementing the value of sempahore */
+
+		if(sem->value <=0)
+		{
+			TCB_t *temp;               /* deleting the element from semaphore q and putiing it on runQ */ 
+			temp=DelQ(&runQ, sem->semQ);
+			AddQ(runQ,temp);
+		}
+ 
+}
+
